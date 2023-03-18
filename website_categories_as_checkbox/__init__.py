2b7efcfe86b561892f6cbba3dd91fbf399410c30
Albin Gilles <agi@niboo.com>
2023-03-17 22:39:23 -0400

    Init v16

--- website_categories_as_checkbox/__init__.py
+++
@@ -1,5 +0,0 @@
-# © 2022 Alexandre Dutry, Nico Darnis
-# © 2022 Niboo SRL (<https://www.niboo.com/>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from . import controllers
--- website_categories_as_checkbox/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# © 2021 Alexandre Dutry
-# © 2021 Niboo SRL (<https://www.niboo.com/>)
-# License Other proprietary.
-
-from . import portal
